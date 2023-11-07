55d1ec384a887e2b440fb8c9502de91cfb13db6c
ebrahim <ebid.py@gmail.com>
2023-11-07 10:26:50 +0330

    requirements installed

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="hairBrushDetection", 
+    version="0.0.0", 
+    author="ebrahim", 
+    author_email="ebid.py@gmail.com", 
+    packages=find_packages(), 
+    install_requires=[]
+)
