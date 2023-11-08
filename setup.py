04cc77654773c4a3cfa7d748768f53f812be8e92
ebrahim <ebid.py@gmail.com>
2023-11-08 09:47:21 +0330

    structure changed

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     version="0.0.0", 
     author="ebrahim", 
     author_email="ebid.py@gmail.com", 
-    packages=find_packages(), 
+    package_dir={"": "src"}, 
+    packages=find_packages(where="src/"), 
     install_requires=[]
 )
--- template.py
+++ template.py
@@ -7,28 +7,29 @@ logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
 project_name = "hairBrushDetector"
 
 list_of_files = [
-    ".github/workflows/.gitkeeep", 
-    "data/.gitkeep", 
-    f"{project_name}/components/__init__.py", 
-    f"{project_name}/constants/__init__.py", 
-    f"{project_name}/constants/__init__.py",
-    f"{project_name}/constants/training_pipline/__init__.py",
-    f"{project_name}/constants/application.py",
-    f"{project_name}/entity/config_entity.py",
-    f"{project_name}/entity/artifacts_entity.py",
-    f"{project_name}/exception/__init__.py",
-    f"{project_name}/logger/__init__.py",
-    f"{project_name}/pipeline/__init__.py",
-    f"{project_name}/constants/training_pipeline.py",
-    f"{project_name}/utils/__init__.py",
-    f"{project_name}/utils/main_utils.py",
-    "research/trials.ipynb", 
-    "app.py", 
-    "Dockerfile", 
-    "requirements.txt", 
-    "setup.py"
-]
+    ".github/workflows/.gitkeep",
+    "data/.gitkeep",
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/logger/__init__.py",
+    f"src/{project_name}/exception/__init__.py",
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/config/__init__.py",
+    f"src/{project_name}/config/configuration.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/constants/__init__.py",
+    "config/config.yaml",
+    "dvc.yaml",
+    "params.yaml",
+    "requirements.txt",
+    "setup.py",
+    "main.py", 
+    "app.py",
+    "Dockerfile",
+    "research/trials.ipynb",
 
+]
 for filepath in list_of_files:
     filedir, filename = os.path.split(filepath)
     
